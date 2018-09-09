d7e9171b56db5486bfe7ca4cedbadcbddd55c7a4
hasegawa-takashi <kamikirihien@gmail.com>
2018-09-10 00:12:37 +0900

    空間の広さを認知するためのレイ処理を簡単に実装、dll側の調整中

--- Assets/thasepon/htAudio/htAroundInfo.cs
+++ Assets/thasepon/htAudio/htAroundInfo.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class htAroundInfo : MonoBehaviour {
 
     struct AroundInfo{
-        Vector3 Hitpoint;
-        Material HitObjMat;
-        float Dist;
+        public Vector3 Hitpoint;
+        public Material HitObjMat;
+        public float Dist;
     };
 
     // レイの設定用変数
@@ -26,15 +26,18 @@ public class htAroundInfo : MonoBehaviour {
     float RayDist = 1000.0f;
     RaycastHit hits;
     public LayerMask mask;
-    
+
+    AroundInfo[] PosInfo;
+
     // Use this for initialization
     void Start () {
         Rays = new Ray[(int)RayDir.MAX_RAYDIR];
+        PosInfo = new AroundInfo[(int)RayDir.MAX_RAYDIR];
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        RayUpdate();
     }
 
     /// <summary>
@@ -57,14 +60,23 @@ public class htAroundInfo : MonoBehaviour {
             Rays[i].origin = transform.position;
         }
 
+        // ナビの判定
+        if (!Physics.Raycast(Rays[(int)RayDir.Navi], out hits, RayDist, mask))
+        {
+            return;
+        }
+
         // ヒットしているレイの数値の保存
         for (int i = 0; i < (int)RayDir.MAX_RAYDIR; i++)
         {
             if (Physics.Raycast(Rays[i], out hits, RayDist, mask))
             {
-                // レイの数値の保存
-                Debug.Log(hits.transform.name);
+                // レイの表示をデバッグ
                 Debug.DrawRay(Rays[i].origin, Rays[i].direction, Color.green);
+
+                // ヒットしたレイの情報を保存
+                PosInfo[i].Hitpoint = hits.point;
+                PosInfo[i].HitObjMat = hits.transform.GetComponent<Material>();
             }
         }
 
